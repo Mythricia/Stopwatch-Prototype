ab6173d66e256c7188ad118b4f5ec19d9ff127dd
Haatveit <mythricia@gmail.com>
2016-08-05 02:54:51 +0200

    More tweaks

--- Assets/Scripts/DebugAndDevelopment/LevelSpawnEffect.cs
+++ Assets/Scripts/DebugAndDevelopment/LevelSpawnEffect.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class LevelSpawnEffect : MonoBehaviour {
+public class LevelSpawnEffect : MonoBehaviour
+{
+    Renderer[] rendArray;
+    public float delayBetweenPop = 0.1f;
+    int progressIndex = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void Start()
+    {
+        // rendArray = GetComponentsInChildren<Renderer>();
+        rendArray = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer rend in rendArray)
+        {
+            rend.enabled = false;
+        }
+
+        shuffle(rendArray);
+
+        InvokeRepeating("EnableNextRenderer", 0f, delayBetweenPop);
+    }
+
+
+    void EnableNextRenderer()
+    {
+        if (progressIndex < rendArray.Length)
+        {
+            rendArray[progressIndex].enabled = true;
+            progressIndex++;
+        }
+        else
+        {
+            CancelInvoke();
+        }
+    }
+
+
+    void shuffle(object[] items)
+    {
+        for (int t = 0; t < items.Length; t++)
+        {
+            object tmp = items[t];
+            int r = Random.Range(t, items.Length);
+            items[t] = items[r];
+            items[r] = tmp;
+        }
+    }
 }
